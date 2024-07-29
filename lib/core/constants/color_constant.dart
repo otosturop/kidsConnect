9e2524a531823f342bcc911453a140fa70f8b3e3
otosturop <mehmetarikan.ma@gmail.com>
2024-07-29 22:57:49 +0300

    color changed

--- lib/core/constants/color_constant.dart
+++ lib/core/constants/color_constant.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 //colors
 const Color myPrimaryColor = Color(0XFF395fd6);
 const Color myPrimaryContainer = Color(0XFF002987);
-const Color mySecondaryColor = Color(0XFF002987);
+const Color mySecondaryColor = Color(0XFF443AD6);
 const Color mySecondaryContainer = Color(0XFF6E95A0);
-const Color myTertiary = Color(0XFF6E95A0);
+const Color myTertiary = Color(0XFF3A92D6);
 
-const Color myOnPrimaryColor = Color(0XFF181A22);
+const Color myOnPrimaryColor = Color(0XFF783AD6);
 const Color myOnSecondaryColor = Color(0xFFEDF0F6);
 
 const Color myTextBlackColor = Color(0XFF000000);
